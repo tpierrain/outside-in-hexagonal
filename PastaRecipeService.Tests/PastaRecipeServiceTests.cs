7ed88d20272a3a99bf2ac7a20cef7d478476faf1
Thomas PIERRAIN <thomas@pierrain.net>
2015-05-24 19:40:19 +0200

    Add first failing test: ShouldProvideTheRecipeGivenAPastaName()

---
+++ PastaRecipeService.Tests/PastaRecipeServiceTests.cs
@@ -0,0 +1,37 @@
+﻿namespace PastaRecipeService.Tests
+{
+    using System;
+    using System.Collections.Generic;
+
+    using NFluent;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class RecipeServiceTests
+    {
+        [Test]
+        public void ShouldProvideTheRecipeGivenAPastaName()
+        {
+            var recipeService = new RecipeService();
+            var gnocchiRecipe = recipeService.GetRecipeFor(pastaName: "gnocchi");
+            Check.That(gnocchiRecipe.Ingredients).Contains("eggs", "potatoes", "flour");
+            Check.That(gnocchiRecipe.PastaName).IsEqualTo("gnocchi");
+        }
+    }
+
+    public class RecipeService
+    {
+        public PastaRecipe GetRecipeFor(string pastaName)
+        {
+            return null;
+        }
+    }
+
+    public class PastaRecipe
+    {
+        public string PastaName { get; private set; };
+
+        public IEnumerable<string> Ingredients { get; private set; } 
+    }
+}

b47b09cd6b52928aaac4ddf26afce401552443a0
Thomas PIERRAIN <thomas@pierrain.net>
2015-05-25 17:02:43 +0200

    Move the CommandLineAdapter to the PastaRecipe.Infra directory

--- PastaRecipe.Console/Program.cs
+++ PastaRecipe.Console/Program.cs
@@ -3,6 +3,7 @@
 namespace PastaRecipe.Console
 {
     using PastaRecipe.Domain;
+    using PastaRecipe.Infra;
 
     class Program
     {
@@ -24,31 +25,11 @@ namespace PastaRecipe.Console
                     break;
                 }
                 
-                var pastaRecipe = commandLineArgumentAdapter.RequestRecipeFor(enteredValue);
+                var retrievedRecipe = commandLineArgumentAdapter.RequestRecipeFor(enteredValue);
 
-                System.Console.WriteLine("The recipe for the pasta '{0}' is '{1}'", pastaRecipe.PastaName, pastaRecipe.Ingredients);
+                System.Console.WriteLine(retrievedRecipe);
                 System.Console.WriteLine(Environment.NewLine);
             }
-        }
-    }
-
-    internal class CommandLineAdapter
-    {
-        private readonly RecipeService pastaRecipeService;
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="CommandLineAdapter"/> class.
-        /// </summary>
-        /// <param name="pastaRecipeService">The pasta recipe service.</param>
-        public CommandLineAdapter(RecipeService pastaRecipeService)
-        {
-            this.pastaRecipeService = pastaRecipeService;
-        }
-
-        public PastaRecipe RequestRecipeFor(string pastaName)
-        {
-            // the simplest possible adapter on earth here; no need to translate model
-            return this.pastaRecipeService.GetRecipeFor(pastaName);
         }
     }
 }
---
+++ PastaRecipe/Infra/CommandLineAdapter.cs
@@ -0,0 +1,36 @@
+namespace PastaRecipe.Infra
+{
+    using System.Linq;
+
+    using PastaRecipe.Domain;
+
+    public class CommandLineAdapter
+    {
+        private readonly RecipeService pastaRecipeService;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CommandLineAdapter"/> class.
+        /// </summary>
+        /// <param name="pastaRecipeService">The pasta recipe service.</param>
+        public CommandLineAdapter(RecipeService pastaRecipeService)
+        {
+            this.pastaRecipeService = pastaRecipeService;
+        }
+
+        public string RequestRecipeFor(string pastaName)
+        {
+            // the simplest possible adapter on earth here; no need to translate model
+            var recipe = this.pastaRecipeService.GetRecipeFor(pastaName);
+
+            // adapt from the domain model to the output representation
+            var output = string.Format("The recipe for the pasta '{0}' is '{1}'", recipe.PastaName, this.PrintIngredients(recipe));
+
+            return output;
+        }
+
+        private string PrintIngredients(PastaRecipe recipe)
+        {
+            return string.Join(", ", recipe.Ingredients.ToArray());
+        }
+    }
+}

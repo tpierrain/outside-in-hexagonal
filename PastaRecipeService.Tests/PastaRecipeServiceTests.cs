6d1250463166724d40ab61cf838c739c1bf8d600
Thomas PIERRAIN <thomas@pierrain.net>
2015-05-25 13:20:42 +0200

    Make the first test green

--- PastaRecipeService.Tests/PastaRecipeServiceTests.cs
+++ PastaRecipeService.Tests/PastaRecipeServiceTests.cs
@@ -1,6 +1,5 @@
 ﻿namespace PastaRecipeService.Tests
 {
-    using System;
     using System.Collections.Generic;
 
     using NFluent;
@@ -22,16 +21,49 @@
 
     public class RecipeService
     {
+        private Dictionary<string, PastaRecipe> recipes;
+
+        public RecipeService()
+        {
+            this.InitializeRecipes();
+        }
+
+        private void InitializeRecipes()
+        {
+            this.recipes = new Dictionary<string, PastaRecipe>
+                               {
+                                   { "gnocchi", PastaRecipe.Parse("gnocchi(eggs-potatoes-flour)") },
+                                   { "spaghetti", PastaRecipe.Parse("spaghetti(eggs-flour)") },
+                                   { "organic spaghetti", PastaRecipe.Parse("organic spaghetti(organic eggs-flour)") },
+                                   { "spinach farfalle", PastaRecipe.Parse("spinach farfalle(eggs-flour-spinach)") },
+                                   { "tagliatelle", PastaRecipe.Parse("tagliatelle(eggs-flour)") }
+                               };
+        }
+
         public PastaRecipe GetRecipeFor(string pastaName)
         {
-            return null;
+            return this.recipes[pastaName];
         }
     }
 
     public class PastaRecipe
     {
-        public string PastaName { get; private set; };
+        public string PastaName { get; private set; }
 
-        public IEnumerable<string> Ingredients { get; private set; } 
+        public IEnumerable<string> Ingredients { get; private set; }
+
+        private PastaRecipe(string pastaName, IEnumerable<string> ingredients)
+        {
+            this.PastaName = pastaName;
+            this.Ingredients = ingredients;
+        }
+
+        public static PastaRecipe Parse(string pastaNameAndRecipe)
+        {
+            var pastaName = pastaNameAndRecipe.Substring(0, pastaNameAndRecipe.IndexOf('('));
+            var ingredientsList = pastaNameAndRecipe.Substring(pastaNameAndRecipe.IndexOf('(') + 1, pastaNameAndRecipe.Length - pastaNameAndRecipe.IndexOf('(') - 2);
+
+            return new PastaRecipe(pastaName, ingredientsList.Split('-'));
+        }
     }
 }
---
+++ PastaRecipeService.Tests/PastaRecipeTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PastaRecipeService.Tests
+{
+    using NFluent;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class PastaRecipeTests
+    {
+        [Test]
+        public void ShouldParsePastaLine()
+        {
+            const string PastaLine = "spinach farfalle(eggs-flour-spinach)";
+
+            var recipe = PastaRecipe.Parse(PastaLine);
+            Check.That(recipe.PastaName).IsEqualTo("spinach farfalle");
+            Check.That(recipe.Ingredients).Contains("eggs", "flour", "spinach");
+        }
+    }
+}
